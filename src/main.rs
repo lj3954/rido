ef97a6f99b662c89f00fe9e3f79416815afb7acd
Liam <33645555+lj3954@users.noreply.github.com>
2024-04-21 15:37:13 -0700

    Include license in binary

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // This is a very basic CLI, mainly intended to be used in non-rust applications.
 
+const LICENSE: &str = include_str!("../LICENSE");
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let (release, language, arch) = match args.len() {
@@ -7,10 +9,27 @@ fn main() {
         3 => (args[1].as_str(), args[2].as_str(), "x86_64"),
         4 => (args[1].as_str(), args[2].as_str(), args[3].as_str()),
         _ => {
+            println!("This program is free software: you can redistribute it and/or modify it under the 
+terms of the GNU General Public License as published by the Free Software Foundation,  either 
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If 
+not, see <https://www.gnu.org/licenses/>.
+
+For more information, run {} --license\n", args[0]);
             eprintln!("Usage: {} [language] [arch]", args[0]);
             std::process::exit(1);
         }
     };
+
+    if release == "--license" {
+        println!("{}", LICENSE);
+        std::process::exit(0);
+    }
 
     match rido::WindowsRelease::new(release, language, arch) {
         Ok(release) => println!("{} {}", &release.url, if let Some(hash) = &release.hash { hash } else { "" }),
